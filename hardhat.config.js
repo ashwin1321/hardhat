968e5581fb33756ddb8cf43fe814b99fb99305d3
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-28 21:28:31 +0545

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,18 +8,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   for (const account of accounts) {
     const address = await account.getAddress();
     const balance = await account.getBalance();
+    console.log(account);
     console.log(` ${address} : ${hre.ethers.utils.formatEther(balance)}`);      // converting wei to ether
   }
 });
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.17",
+  // defaultNetwork: "goerli",
   defaultNetwork: "hardhat",
-  nework: {
-    hardhat: {}
-  }
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/6d339ca37c3f48cabe9762fdbd1d58c2",              // infura project id
+      accounts: ["8f02f63e53f06e210145d1fbd542ce969aa185a4d99e3cadbac315ebf29976ee"]    // private key of the account from metamask
+    }
+  },
 };
 
 // Path: scripts/sample-script.js
